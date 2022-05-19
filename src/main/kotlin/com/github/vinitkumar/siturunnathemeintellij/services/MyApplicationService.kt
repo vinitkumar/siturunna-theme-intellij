868689974f71a413c084a1da359a00b29f0b1981
GitHub Action <action@github.com>
2022-05-19 17:10:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vinitkumar/siturunnathemeintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vinitkumar.siturunnathemeintellij.services
+
+import com.github.vinitkumar.siturunnathemeintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
